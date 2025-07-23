c6a9e317e3f98b786290a52402be8e5580b9698a
Francisco Jesus Lorenzo Nombela <frlorenz@c1r9s4.42madrid.com>
2025-07-23 18:35:04 +0200

    trabajando el CD

--- src/build_ins/cd.c
+++ src/build_ins/cd.c
@@ -6,34 +6,29 @@
 /*   By: frlorenz <frlorenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 11:49:44 by frlorenz          #+#    #+#             */
-/*   Updated: 2025/07/23 12:57:09 by frlorenz         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:34:18 by frlorenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int ft_fillpwd(t_env **pwd, t_env **old_pwd, t_env **home, t_env **env)
+static void ft_swap_pwd(t_env *pwd, t_env *old_pwd, char *temp)
 {
-    int i;
-
-    i = 0;
-    *pwd = search_node(env, "PWD");
-    if (!*pwd)
-        i = 1;
-    *old_pwd = search_node(env, "OLDPWD");
-    if (!*old_pwd)
-        i = 2;
-    *home = search_node(env, "HOME");
-    if (!*home)
-        i = 3;
-    return(i);
+    if(!old_pwd)
+        free(temp);
+    else
+        old_pwd->content = temp;
+    if (pwd != NULL)
+        pwd->content = getcwd(NULL, 0);
 }
 
-static void ft_cd_home(t_env *pwd, t_env *old_pwd, t_env *home, int check)
+static void ft_cd_home(t_env *pwd, t_env *old_pwd, t_env *home)
 {
     char *temp;
     
-    if (check == 3)
+    if (!home)
+        ft_putstr_fd("cd: not set HOME\n", 2);    
+    else
     {
         if (chdir(home->content) == -1)
         {
@@ -43,74 +38,89 @@ static void ft_cd_home(t_env *pwd, t_env *old_pwd, t_env *home, int check)
         }       
         else
         {
-            temp = pwd->content;
-            pwd->content = home->content;
-            old_pwd->content = temp;
+            if (pwd != NULL)
+            {
+                if (old_pwd != NULL)
+                {
+                    temp = pwd->content;
+                    old_pwd->content = temp;
+                }
+                pwd->content = home->content;
+            }  
         }
-    }
-    else
-        ft_putstr_fd("cd: not set HOME\n", 2);
+    }    
 }
 
-static void ft_oldpwd(t_env *pwd, t_env *old_pwd, int check)
+static void ft_oldpwd(t_env *pwd, t_env *old_pwd)
 {
     char *temp;
 
-    if (check == 2)
+    if (!old_pwd)
+        ft_putstr_fd("cd: not set OLDPWD\n", 2);
+    else
     {
         if (chdir(old_pwd->content) == -1)
-            perror("cd");
+        {
+            ft_putstr_fd("cd: ", 2);
+            ft_putstr_fd(old_pwd->content, 2);
+            ft_putstr_fd(": No such file or directory\n", 2);
+        }
         else
         {
-            temp = pwd->content;
-            pwd->content = getcwd(NULL, 0);
-            printf("%s\n", pwd->content);
-            old_pwd->content = temp;
+            if (pwd != NULL)
+            {
+                temp = pwd->content;
+                pwd->content = getcwd(NULL, 0);
+                printf("%s\n", pwd->content);
+                if (old_pwd != NULL)
+                    old_pwd->content = temp;
+            }
         }
     }
-    else
-        ft_putstr_fd("cd: not set OLDPWD\n", 2);
+        
 }
 
-static void ft_commond_cd(char *path, t_env *pwd, t_env *old_pwd, int check, t_env **env)
+static void ft_commond_cd(char *path, t_env *pwd, t_env *old_pwd)
 {
     char *temp;
     
+    temp = getcwd(NULL, 0);
     if (chdir(path) == -1)
     {
         ft_putstr_fd("cd: ", 2);
         ft_putstr_fd(path, 2);
         ft_putstr_fd(" No such file or directory\n", 2);
     }
-    if (check == 0)
+    else
+    {
+        ft_swap_pwd(pwd, old_pwd, temp);
+    }
+    if (pwd != NULL)
     {
         temp = pwd->content;
         pwd->content = getcwd(NULL, 0);
-        old_pwd->content = temp;
-    }
-    else if (check == 1)
-    {
-        env_add_last(env, new_node("OLDPWD", pwd->content));
-        pwd->content = getcwd(NULL, 0);
+        if (old_pwd != NULL)
+            old_pwd->content = temp;
     }
 }
 
 void cd(t_list *arg, t_env *env)
 {
-    int check;
     t_env *pwd;
     t_env *old_pwd;
     t_env *home;
 
-    check = ft_fillpwd(&pwd, &old_pwd, &home, &env);
+    pwd = search_node(&env, "PWD");
+    old_pwd = search_node(&env, "OLDPWD");
+    home = search_node(&env, "HOME");
     if(!arg)
-        ft_cd_home(pwd, old_pwd, home, check);           
+        ft_cd_home(pwd, old_pwd, home);           
     else if(ft_lstsize(arg) == 1)
     {
         if (ft_strcmp((char *)arg->content, "-") == 0)
-            ft_oldpwd(pwd, old_pwd, check); 
+            ft_oldpwd(pwd, old_pwd); 
         else
-            ft_commond_cd((char *)arg->content, pwd, old_pwd, check, &env);
+            ft_commond_cd((char *)arg->content, pwd, old_pwd);
     }
     else
         ft_putstr_fd("cd: too many arguments\n", 2);
