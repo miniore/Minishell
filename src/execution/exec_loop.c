e15c0fd90b8d2f1e9067ed09b8d9311c38a5aad9
Francisco Jesus Lorenzo Nombela <frlorenz@c1r11s6.42madrid.com>
2025-08-11 15:26:11 +0200

    quizas ya esta arreglado

--- src/execution/exec_loop.c
+++ src/execution/exec_loop.c
@@ -86,12 +86,6 @@ void exec_pipes(t_backpack *backpack, char **envp, t_env *path)
                 }
                 else
                     close(prev_fd);
-                if (backpack->n < (int)backpack->commands_nb - 1)
-                {
-                    close(pipe_fd[0]);
-                    dup2(pipe_fd[1], STDOUT_FILENO);
-                    close(pipe_fd[1]);
-                }
                 if (ft_exec_redir(backpack->commands_lst[backpack->n].redirection) != 0)
                 {
                     ft_putstr_fd("Error de archivo\n", 2);
@@ -110,6 +104,17 @@ void exec_pipes(t_backpack *backpack, char **envp, t_env *path)
                 //     dup2(pipe_fd[1], STDOUT_FILENO);
                 //     close(pipe_fd[1]);
                 // }
+                if (ft_n_hdocs(redirection) == 0 && backpack->n < (int)backpack->commands_nb - 1)
+                {
+                    close(pipe_fd[0]);
+                    dup2(pipe_fd[1], STDOUT_FILENO);
+                    close(pipe_fd[1]);
+                }
+                else
+                {
+                    close(pipe_fd[0]);
+                    close(pipe_fd[1]);
+                }
                 // if ((ft_strcmp(redirection->op, "<")  == 0 || ft_strcmp(redirection->op, "<<")  == 0) &&
                 //         backpack->n < (int)backpack->commands_nb - 1)
                 // {
--- src/redirection/redir_tools.c
+++ src/redirection/redir_tools.c
@@ -6,7 +6,7 @@
 /*   By: frlorenz <frlorenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 18:59:11 by frlorenz          #+#    #+#             */
-/*   Updated: 2025/08/09 21:26:55 by frlorenz         ###   ########.fr       */
+/*   Updated: 2025/08/11 15:21:34 by frlorenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int ft_n_hdocs(t_redir *redirection)
     i = 0;
     while(redirection)
     {
-        printf("||((%s En_Linea %d))||=> %s\n", __FILE__,__LINE__, "ERROR");
         if(ft_strcmp(redirection->op, "<<")  == 0)
             i++;
         redirection = redirection->next;
     }
-    printf("||((%s En_Linea %d))||=> %i\n", __FILE__,__LINE__, i);
     return(i);
 }
